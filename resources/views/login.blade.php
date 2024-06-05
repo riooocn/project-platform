c0cf172ead06f0b42af9f08f0ef3b203114d60d4
riooocn <davidriochristian43@gmail.com>
2024-06-05 10:36:35 +0700

    merapikan halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,28 +15,7 @@
     </style>
 </head>
 <body>
-   <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
+ 
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
