856f1a271a00c5af806b8832b786dcde297de6c8
riooocn <davidriochristian43@gmail.com>
2024-06-06 07:06:33 +0700

    Memperbaiki tampilan warna dari database

--- resources/views/men.blade.php
+++ resources/views/men.blade.php
@@ -18,7 +18,7 @@
                       {{ $product['name'] }}
                     </a>
                   </h3>
-                  <p class="mt-1 text-sm text-gray-500">Black</p>
+                  <p class="mt-1 text-sm text-gray-500">{{ $product['color'] }}</p>
                 </div>
                 <p class="text-sm font-medium text-gray-900">IDR {{ number_format($product->price, 2, ',', '.') }}</p>
               </div>
